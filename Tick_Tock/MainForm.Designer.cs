d20c261385685dcc43a9ebe839b33d04075317d1
LeemLime <mail@liam-wright.co.uk>
2017-09-06 13:25:37 +0100

    Initial Commit

---
+++ Tick_Tock/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Tick_Tock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.ActiveTimer = new System.Windows.Forms.Timer(this.components);
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(97, 74);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(75, 23);
+            this.PauseButton.TabIndex = 0;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.ForeColor = System.Drawing.Color.Lime;
+            this.StatusLabel.Location = new System.Drawing.Point(91, 21);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(89, 31);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "Active";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 137);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.PauseButton);
+            this.Name = "MainForm";
+            this.Text = "Tick Tock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Timer ActiveTimer;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ Tick_Tock/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tick_Tock
+{
+    public partial class MainForm : Form
+    {
+        public static Timer timer;
+        bool pauseButtonClicked = false;
+        [DllImport("User32.dll",
+                    EntryPoint = "mouse_event",
+                    CallingConvention = CallingConvention.Winapi)]
+                    internal static extern void Mouse_Event(int dwFlags,
+                                                            int dx,
+                                                            int dy,
+                                                            int dwData,
+                                                            int dwExtraInfo);
+
+        [DllImport("User32.dll",
+                   EntryPoint = "GetSystemMetrics",
+                   CallingConvention = CallingConvention.Winapi)]
+
+        internal static extern int InternalGetSystemMetrics(int value);
+
+        public MainForm()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            
+
+            //ActiveTimeLabel.Text = stopWatch.Elapsed.ToString();
+           // stopWatch.Start();
+            /// <summary>
+            /// The main entry point for the application.
+            /// </summary>
+
+            // Move mouse cursor to an absolute position to_x, to_y and make left button click:
+            int to_x = 500;
+            int to_y = 300;
+
+            int screenWidth = InternalGetSystemMetrics(0);
+            int screenHeight = InternalGetSystemMetrics(1);
+
+            // Mickey X coordinate
+            int mic_x = (int)System.Math.Round(to_x * 65536.0 / screenWidth);
+            // Mickey Y coordinate
+            int mic_y = (int)System.Math.Round(to_y * 65536.0 / screenHeight);
+
+
+
+
+            timer = new Timer();
+            //timer.interval = 30 Seconds;
+            timer.Interval = (int)(TimeSpan.TicksPerMinute * 0.5 / TimeSpan.TicksPerMillisecond);
+            
+            timer.Tick += (sender, args) =>
+            {
+
+                // 0x0001 | 0x8000: Move + Absolute position
+                Mouse_Event(0x0001 | 0x8000, mic_x, mic_y, 0, 0);
+                // 0x0002: Left button down
+                //Mouse_Event(0x0002, mic_x, mic_y, 0, 0);
+                // 0x0004: Left button up
+               // Mouse_Event(0x0004, mic_x, mic_y, 0, 0);
+
+            };
+            timer.Start();
+
+
+            InitializeComponent();
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            if (pauseButtonClicked)
+            {
+                pauseButtonClicked = false;
+                timer.Start();
+                PauseButton.Text = "Pause";
+                StatusLabel.ForeColor = Color.Lime;
+                StatusLabel.Text = "Active";
+            }
+           
+            else
+            {
+                pauseButtonClicked = true;
+                timer.Stop();
+                PauseButton.Text = "Start";
+                StatusLabel.ForeColor = Color.Red;
+                StatusLabel.Text = "Paused";
+            }
+                
+
+        }
+    }
+}
